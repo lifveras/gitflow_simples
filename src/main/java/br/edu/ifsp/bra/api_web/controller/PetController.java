bf537bf8525cffe8838e20ffcc5c9ad37d594a22
unknown <gustavo_veras@ifsp.edu.br>
2024-09-20 14:31:50 -0300

    Adição de model e controller

---
+++ src/main/java/br/edu/ifsp/bra/api_web/controller/PetController.java
@@ -0,0 +1,15 @@
+package br.edu.ifsp.bra.api_web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PetController {
+}
+
+// Comandos para subir ao GitHub
+// 1. git add .
+// 2. git commit -m "Adição de model e controller"
+// 3. git push origin
+
+// Comandos para os colaboradores
+// 1. git pull
