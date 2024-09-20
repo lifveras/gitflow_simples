c3f95da05e8097f72bd8f7d0cd37a50735baa1a4
unknown <gabriellybabi10@gmail.com>
2024-09-20 15:12:58 -0300

    Adição Lista de Pets

--- src/main/java/br/edu/ifsp/bra/api_web/ApiWebApplication.java
+++ src/main/java/br/edu/ifsp/bra/api_web/ApiWebApplication.java
@@ -8,6 +8,7 @@ public class ApiWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiWebApplication.class, args);
+		
 	}
 
 }
--- src/main/java/br/edu/ifsp/bra/api_web/controller/PetController.java
+++ src/main/java/br/edu/ifsp/bra/api_web/controller/PetController.java
@@ -1,15 +1,29 @@
 package br.edu.ifsp.bra.api_web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifsp.bra.api_web.model.Pet;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class PetController {
+    List<Pet> listaDePets = new ArrayList<> ();
+
+    public PetController(){
+        listaDePets.add(new Pet("Mingal", "Gato", 1000000));
+        listaDePets.add(new Pet("Lana", "Cachorro", 1000000));
+        listaDePets.add(new Pet("Pipoca", "Coelha", 1000000));
+    }
+
+    @GetMapping("/pet")
+    public List<Pet> getAllPets() {
+        return listaDePets;
+    }   
+    
 }
 
-// Comandos para subir ao GitHub
-// 1. git add .
-// 2. git commit -m "Adição de model e controller"
-// 3. git push origin
-
-// Comandos para os colaboradores
-// 1. git pull
