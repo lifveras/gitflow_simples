98b47f991641152359dd0fac6b28d71c607e86fb
unknown <gustavo_veras@ifsp.edu.br>
2024-09-20 14:17:24 -0300

    Adição projeto Spring

---
+++ src/test/java/br/edu/ifsp/bra/api_web/ApiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.bra.api_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
