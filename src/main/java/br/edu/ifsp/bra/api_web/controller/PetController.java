4149d842cdb801ad6a80b084a4528c22950439d7
Professor <gustavo_veras@ifsp.edu.br>
2024-09-26 14:35:18 -0300

    Revertido

--- src/main/java/br/edu/ifsp/bra/api_web/controller/PetController.java
+++ src/main/java/br/edu/ifsp/bra/api_web/controller/PetController.java
@@ -1,37 +1,9 @@
 package br.edu.ifsp.bra.api_web.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import br.edu.ifsp.bra.api_web.model.Pet;
-
 
 @RestController
 public class PetController {
-    List<Pet> listaDePets = new ArrayList<>();
-
-    public PetController(){
-        listaDePets.add(
-            new Pet("Teco", "Cachorro",
-            10000)
-        );
-        listaDePets.add(
-            new Pet("Lobo", "Hamster",
-            99999)
-        );
-    }
-
-    // Endpoint que busca um Pet por id
-    @GetMapping("/pet/{id}")
-    public Pet getPetById(
-        @PathVariable("id") Long idPet) 
-    {
-        return listaDePets.get(idPet.intValue() - 1);
-    }
     
 }
 
