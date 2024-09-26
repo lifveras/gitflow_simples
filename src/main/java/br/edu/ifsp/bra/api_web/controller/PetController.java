a25fc797fc004d15a83d41899735645f009e7a93
Professor <gustavo_veras@ifsp.edu.br>
2024-09-26 14:11:24 -0300

    Adição de um POST /pet

--- src/main/java/br/edu/ifsp/bra/api_web/controller/PetController.java
+++ src/main/java/br/edu/ifsp/bra/api_web/controller/PetController.java
@@ -1,9 +1,17 @@
 package br.edu.ifsp.bra.api_web.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifsp.bra.api_web.model.Pet;
 
 @RestController
 public class PetController {
+    @PostMapping("/pet")
+    public Pet createNewPet(@RequestBody Pet newPet){
+        return newPet;
+    }
 }
 
 // Comandos para subir ao GitHub
